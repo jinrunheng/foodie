7fc797732037880dcf9b9c0aa0847478587010c6
jinrunheng <1175088275@qq.com>
2024-04-11 23:29:32 +0800

    使用 hibernate validate 验证用户信息

--- foodie-pojo/src/main/java/com/github/bo/center/CenterUserBO.java
+++ foodie-pojo/src/main/java/com/github/bo/center/CenterUserBO.java
@@ -1,7 +1,9 @@
 package com.github.bo.center;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 /**
@@ -18,10 +20,22 @@ public class CenterUserBO {
     private String username;
     private String password;
     private String confirmPassword;
+
+    @NotBlank(message = "用户昵称不能为空")
+    @Length(max = 12, message = "用户昵称不能超过 12 位")
     private String nickname;
+
+    @Length(max = 12, message = "用户真实姓名不能超过 12 位")
     private String realname;
+
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
     private String mobile;
+
+    @Email
     private String email;
+
+    @Min(value = 0, message = "性别选择不正确")
+    @Max(value = 2, message = "性别选择不正确")
     private Integer sex;
     private Date birthday;
 }
