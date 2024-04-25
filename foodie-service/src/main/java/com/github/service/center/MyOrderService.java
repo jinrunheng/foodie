fa5c9c8f5d9c8cc6e30d7dd0cfebd6dcba5e25b5
jinrunheng <1175088275@qq.com>
2024-04-25 23:45:34 +0800

    完成模拟商家发货接口

--- foodie-service/src/main/java/com/github/service/center/MyOrderService.java
+++ foodie-service/src/main/java/com/github/service/center/MyOrderService.java
@@ -19,4 +19,11 @@ public interface MyOrderService {
      * @return
      */
     PagedGridResult queryUserOrders(String userId, Integer orderStatus, Integer page, Integer pageSize);
+
+    /**
+     * 订单状态 -> 商家发货
+     *
+     * @param orderId
+     */
+    void updateDeliverOrderStatus(String orderId);
 }
